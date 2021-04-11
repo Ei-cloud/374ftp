ef729e7d6748e00d78238c7b1d0982002b70b11b
jordasche <80426378+jordasche@users.noreply.github.com>
2021-04-11 04:00:24 -0400

    Completed get and put

--- ClientFolder/Client.c
+++ ClientFolder/Client.c
@@ -4,33 +4,25 @@
  *		A much improved (still imperfect) version of "cli4.c".
  */
 
-#include <unistd.h>
-#include <stdlib.h>
-#include <sys/types.h>
-#include <sys/socket.h>
+
+
 #include <netinet/in.h> /* struct sockaddr_in, htons, htonl */
 #include <netdb.h>      /* struct hostent, gethostbyname() */
 #include <string.h>
 #include <stdio.h>
-#include <sys/types.h>
-#include <sys/socket.h>
 #include <sys/stat.h>
 #include <sys/wait.h>
-#include <netinet/in.h>
 #include <errno.h>
 #include <unistd.h>
 #include <string.h>
 #include <signal.h>
 #include <stdlib.h>
-#include <stdio.h>
 #include <fcntl.h>
 #include "token.h"
 #define BUFSIZE 200000
-#define FILE_BLOCK_SIZE 512
+#define CHUNK_SIZE 512
 #define FILE_NAME_SIZE 64
 #define SERV_TCP_PORT 40001 /* server's "well-known" port number */
-
-
 
 int getFileSize(char *fileName)
 {
@@ -53,45 +45,120 @@ int getFileSize(char *fileName)
      return fileLen;
 }
 
-int write_nbytes(int sd, char *buf, int nbytes)
+void put(int sd, char *fileName)
 {
-     int nw = 0;
-     int n = 0;
-     int i = 0;
-     for (n = 0; n < nbytes; n += nw)
-     {
-          
-          if ((nw = write(sd, buf + n, nbytes - n)) <= 0)
-
-               return (nw); /* write error */
-     }
-     return n;
-}
-
-void put(int sd, char *fileName){
 
      /*Gets file size*/
      int fileSize = getFileSize(fileName);
-     printf("FILE SIZE: %d\n", fileSize);
      int convertedFileSize = htonl(fileSize);
-     printf("CONVERTED FILE SIZE IN CLIENT IS: %d\n", convertedFileSize);
 
      /*Gets file Name Length*/
      int fileNameLength = strlen(fileName);
      int convertedFileNameLength = htonl(fileNameLength);
 
      /*Writes fileNameLength, fileSize and fileName to the server*/
-     write (sd, &convertedFileNameLength, sizeof(convertedFileNameLength));
-     write(sd, &convertedFileSize, sizeof(convertedFileSize));
+     write(sd, &convertedFileNameLength, sizeof(convertedFileNameLength));
      write(sd, fileName, fileNameLength);
 
-     /*Reads file into buf and writes buf to server*/
+     char opcodeA, opcodeB = 'B';
+     char ack;
+
+     read(sd, &opcodeA, sizeof(opcodeA)); //Read Opcode A
+     if (opcodeA == 'A')
+     {
+          read(sd, &ack, sizeof(ack));
+          if (ack == '0')
+          {
+               write(sd, &opcodeB, sizeof(opcodeB));
+               write(sd, &convertedFileSize, sizeof(convertedFileSize));
+
+               /*Reads file into buf and writes buf to server*/
+               FILE *file;
+               int nr = 0;
+               char buf[CHUNK_SIZE];
+               file = fopen(fileName, "rb");
+               while ((nr = fread(buf, 1, CHUNK_SIZE, file)) > 0)
+               {
+                    if (write(sd, buf, nr) == -1)
+                    {
+                         printf("Error in sending file\n");
+                         break;
+                    }
+               }
+               printf("Successfully sent file: %s\n", fileName);
+          }
+          else if (ack == '1')
+          {
+               printf("File name clash\n");
+          }
+          else if (ack == '2')
+          {
+               printf("Could not create file\n");
+          }
+     }
+}
+
+void get(int sd, char *fileName)
+{
+     int nr, nw, i = 0;
+     int fileSize = 0, fileNameSize = 0;
      FILE *file;
-     char buf[BUFSIZE];
-     file = fopen(fileName, "rb");
-     fread(buf, fileSize, sizeof(unsigned char), file);
-     fclose(file);
-     write(sd, buf, fileSize);
+
+     //Client writes the filenameLength and then the FileName to the server
+     int fileNameLength = strlen(fileName);
+     int convertedFileNameLength = htonl(fileNameLength);
+     write(sd, &convertedFileNameLength, sizeof(convertedFileNameLength));
+     write(sd, fileName, fileNameLength);
+
+     char opcodeA, opcodeB = 'B';
+     char ack;
+
+     read(sd, &opcodeA, sizeof(opcodeA)); //Read Opcode A
+
+     if (opcodeA == 'A')
+     {
+          read(sd, &ack, sizeof(ack));
+          if (ack == '0')
+          {
+               
+               /* Reads file size */
+               read(sd, &fileSize, sizeof(fileSize));
+               fileSize = ntohl(fileSize);
+
+               /* Reads file data */
+               int tempChunkSize = CHUNK_SIZE;
+               if (CHUNK_SIZE > fileSize)
+                    tempChunkSize = fileSize;
+
+               char buf[tempChunkSize];
+               int remainingBytes = fileSize;
+               file = fopen(fileName, "wb");
+               /*While there are still bytes to write*/
+               while (remainingBytes > 0)
+               {
+                    if (tempChunkSize > remainingBytes) //if the chunk size > remaining bytes, reduce the chunk size to remaining bytes
+                         tempChunkSize = remainingBytes;
+
+                    nr = read(sd, buf, tempChunkSize); //read current chunk size from socket
+
+                    if (nr == -1) //if nothing more is read, break
+                         break;
+
+                    nw = fwrite(buf, 1, nr, file); //write the chunk to the file
+
+                    if (nw < nr)
+                         break;
+
+                    remainingBytes -= nw; //subtract bytes written from remaining bytes
+               }
+               printf("Received file: %s\n", fileName);
+               fclose(file);
+          }
+          else if (ack == '1')
+          {
+               printf("File does not exist in server\n");
+          }
+     }
 }
 
 int main(int argc, char *argv[])
@@ -135,29 +202,33 @@ int main(int argc, char *argv[])
 
      /* Declaring variables and opcodes*/
      char *tokens[3];
-     char putOpcode = 'P', quitOpcode = 'Q';
+     char putOpcode = 'P', quitOpcode = 'Q', getOpcode = 'G';
      char userInput[32];
 
+     while (1)
+     {
 
-     while(1){
+          /* Gets Client Input*/
 
-     /* Gets Client Input*/
-     
-     printf("Client Input: ");
-     fgets(userInput, sizeof(userInput), stdin);
-     nr = strlen(userInput);
-     tokenise(userInput, tokens);
+          printf("Client Input: ");
+          fgets(userInput, sizeof(userInput), stdin);
+          nr = strlen(userInput);
+          tokenise(userInput, tokens);
 
-     if(strcmp(tokens[0], "put") == 0){
-          write(sd, &putOpcode, sizeof(char));
-          put(sd, tokens[1]);
-     }else if(strcmp(tokens[0], "exit") == 0)
-     {    
-          write(sd, &quitOpcode, sizeof(quitOpcode));
-          exit(0);
-     }
-
-      printf("sent file: %s\n", tokens[1]);
+          if (strcmp(tokens[0], "put") == 0)
+          {
+               write(sd, &putOpcode, sizeof(char));
+               put(sd, tokens[1]);
+          }
+          else if (strcmp(tokens[0], "get") == 0)
+          {
+               write(sd, &getOpcode, sizeof(getOpcode));
+               get(sd, tokens[1]);
+          }
+          else if (strcmp(tokens[0], "exit") == 0)
+          {
+               write(sd, &quitOpcode, sizeof(quitOpcode));
+               exit(0);
+          }
      }
 }
-
--- ServerFolder/Server.c
+++ ServerFolder/Server.c
@@ -1,180 +1,307 @@
-#include <stdio.h>
-#include<unistd.h>
-#include<stdlib.h>
-#include<sys/stat.h>
-#include<sys/types.h>
+
+
 #include <fcntl.h>
-#include<errno.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <sys/stat.h>
+#include <string.h>     /* strlen(), strcmp() etc */
+#include <errno.h>      /* extern int errno, EINTR, perror() */
+#include <signal.h>     /* SIGCHLD, sigaction() */
+#include <sys/types.h>  /* pid_t, u_long, u_short */
+#include <sys/socket.h> /* struct sockaddr, socket(), etc */
+#include <sys/wait.h>   /* waitpid(), WNOHAND */
+#include <netinet/in.h> /* struct sockaddr_in, htons(), htonl(), */
+                        /* and INADDR_ANY */
 
-#include  <unistd.h>
-#include  <stdlib.h>
-#include  <stdio.h>
-#include  <sys/stat.h>
-#include  <string.h>     /* strlen(), strcmp() etc */
-#include  <errno.h>      /* extern int errno, EINTR, perror() */
-#include  <signal.h>     /* SIGCHLD, sigaction() */
-#include  <sys/types.h>  /* pid_t, u_long, u_short */
-#include  <sys/socket.h> /* struct sockaddr, socket(), etc */
-#include  <sys/wait.h>   /* waitpid(), WNOHAND */
-#include  <netinet/in.h> /* struct sockaddr_in, htons(), htonl(), */
-                         /* and INADDR_ANY */
-
-#define   BUFSIZE         200000
-#define   SERV_TCP_PORT   40001           /* server port no */
-#define   FILE_BLOCK_SIZE   512           /* server port no */
+#define BUFSIZE 200000
+#define SERV_TCP_PORT 40001                 /* server port no */
+#define CHUNK_SIZE 512 /* server port no */ /* server port no */
 
 void claim_children()
 {
-     pid_t pid=1;
-     
-     while (pid>0) { /* claim as many zombies as we can */
-         pid = waitpid(0, (int *)0, WNOHANG); 
-     } 
+     pid_t pid = 1;
+
+     while (pid > 0)
+     { /* claim as many zombies as we can */
+          pid = waitpid(0, (int *)0, WNOHANG);
+     }
 }
 
-int read_nbytes(int sd, char *buf, int nbytes)
-{
-	int nr = 1;
-	int n = 0;
-	for (n=0; (n < nbytes) && (nr > 0); n += nr) {
-		if ((nr = read(sd, buf+n, nbytes-n)) < 0){
-			return (nr); /* read error */
-		}
-	}
-	return (n);
-}
 void daemon_init(void)
-{       
-     pid_t   pid;
+{
+     pid_t pid;
      struct sigaction act;
 
-     if ( (pid = fork()) < 0) {
-          perror("fork"); exit(1); 
-     } else if (pid > 0){
-          printf("PID: %d\n", pid); 
-          exit(0);                  /* parent terminates */
+     if ((pid = fork()) < 0)
+     {
+          perror("fork");
+          exit(1);
+     }
+     else if (pid > 0)
+     {
+          printf("PID: %d\n", pid);
+          exit(0); /* parent terminates */
      }
      /* child continues */
-     setsid();                      /* become session leader */
+     setsid(); /* become session leader */
      //chdir("/");                    /* change working directory */
-     umask(0);                      /* clear file mode creation mask */
+     umask(0); /* clear file mode creation mask */
 
      /* catch SIGCHLD to remove zombies from system */
      act.sa_handler = claim_children; /* use reliable signal */
      sigemptyset(&act.sa_mask);       /* not to block other signals */
-     act.sa_flags   = SA_NOCLDSTOP;   /* not catch stopped children */
-     sigaction(SIGCHLD,(struct sigaction *)&act,(struct sigaction *)0);
+     act.sa_flags = SA_NOCLDSTOP;     /* not catch stopped children */
+     sigaction(SIGCHLD, (struct sigaction *)&act, (struct sigaction *)0);
      /* note: a less than perfect method is to use 
               signal(SIGCHLD, claim_children);
     */
 }
 
-void serve_put(int sd){
-    int nr, nw, i=0;
-    char buf[BUFSIZE];
-    char fileName[32];
-    int fileSize = 0, fileNameSize = 0;
-    FILE *file;
+int getFileSize(char *fileName)
+{
+     FILE *file;
+     int fileLen = -1;
 
+     file = fopen(fileName, "rb");
+     if (!file)
+     {
+          perror("FAIL: ");
+          fprintf(stderr, "Unable to open file %s", fileName);
+          return 1;
+     }
+
+     fseek(file, 0, SEEK_END);
+     fileLen = ftell(file);
+     fseek(file, 0, SEEK_SET);
+     fclose(file);
+     return fileLen;
+}
+
+void serve_put(int sd)
+{
+     int nr, nw, i = 0;
+     char fileName[32];
+     bzero(fileName, sizeof(fileName));
+     int fileSize = 0, fileNameSize = 0;
+     FILE *file;
 
      /*Reads file name size*/
      nr = read(sd, &fileNameSize, sizeof(fileNameSize));
      fileNameSize = ntohl(fileNameSize);
 
-     /*Reads file size*/
-     nr = read(sd, &fileSize, sizeof(fileSize));  
-     int normalFileSize = ntohl(fileSize);
-     fileSize = normalFileSize;
-     
-     /*Reads name file size*/
+     /*Reads fileName*/
      nr = read(sd, fileName, fileNameSize);
-     
-     /*Reads file data*/
-     nr = read(sd, buf, fileSize);
-     
-     /*Writes data to the file*/
-     file = fopen(fileName, "wb");
-     fwrite(buf, fileSize, sizeof(unsigned char), file);
-     fclose(file);
+
+     /*Defines OPCODES and ACKCODES*/
+     char opcodeA = 'A', opcodeB, ackSuccess = '0', ackfileExists = '1', ackCannotCreateFile = '2';
+
+     /*Writes opcode A to client*/
+     nw = write(sd, &opcodeA, sizeof(opcodeA));
+
+
+
+
+     /*Checks if filename exists*/
+     if (access(fileName, F_OK) == 0)
+     {
+          nw = write(sd, &ackfileExists, sizeof(ackfileExists)); //writes ackfileExists to client
+          return;
+     }
+
+     /*Checks if file can be created*/
+     if ((file = fopen(fileName, "wb")) == NULL)
+     {
+          nw = write(sd, &ackCannotCreateFile, sizeof(ackCannotCreateFile)); //writes ackCannotCreateFile to client
+          fclose(file);
+          return;
+     }
+     /*If no issues, write success ack*/
+     nw = write(sd, &ackSuccess, sizeof(ackSuccess));
+
+
+
+
+
+     /*read opcode B from client and continue if it is received*/
+     nr = read(sd, &opcodeB, sizeof(opcodeB));
+
+     if (opcodeB == 'B')
+     {
+          /*Reads file size*/
+          nr = read(sd, &fileSize, sizeof(fileSize));
+          int normalfileSize = ntohl(fileSize);
+          fileSize = normalfileSize;
+
+          /*Reads file data*/
+          int tempChunkSize = CHUNK_SIZE;
+          if (CHUNK_SIZE > fileSize)
+               tempChunkSize = fileSize;
+
+          char buf[tempChunkSize];
+          int remainingBytes = fileSize;
+
+          /*While there are still bytes to write*/
+          while (remainingBytes > 0)
+          {
+               if (tempChunkSize > remainingBytes) //if the chunk size > remaining bytes, reduce the chunk size to remaining bytes
+                    tempChunkSize = remainingBytes;
+
+               nr = read(sd, buf, tempChunkSize); //read current chunk size from socket
+
+               if (nr == -1) //if nothing more is read, break
+                    break;
+
+               nw = fwrite(buf, 1, nr, file); //write the chunk to the file
+
+               if (nw < nr)
+                    break;
+
+               remainingBytes -= nw; //subtract bytes written from remaining bytes
+          }
+          fclose(file);
+     }
 }
 
-void serve_a_client(int sd){
-  char opcode;
+void serve_get(int sd)
+{
+     int nr, nw;
+     //Read the file name lenght from the Client
+     char fileName[32];
+     bzero(fileName, sizeof(fileName));
+     int fileNameLen;
 
-  /*Reads OPCODES and runs appropriate functions*/
-  while(read(sd, &opcode, sizeof(opcode))){
-       printf("OPCODE IS: %c", opcode);
-       if(opcode == 'P'){
-          serve_put(sd);
-       }else if(opcode == 'Q'){
-            break;
-       }
-  }
-  printf("SERVED CLIENT\n");
-  return;
+     read(sd, &fileNameLen, sizeof(fileNameLen));
+     fileNameLen = ntohl(fileNameLen);
+     read(sd, fileName, fileNameLen);
 
+     /*Defines OPCODES and ACKCODES*/
+     char opcodeA = 'A', opcodeB, ackSuccess = '0', ackfileDoesNotExist = '1';
 
+     write(sd, &opcodeA, sizeof(opcodeA));
+
+     if (access(fileName, F_OK) == 0)
+     {
+          nw = write(sd, &ackSuccess, sizeof(ackSuccess)); //writes ackSuccess to client
+          //Server sends the file size
+          int fileSize = getFileSize(fileName);
+          int convertedFileSize = htonl(fileSize);
+          write(sd, &convertedFileSize, sizeof(convertedFileSize));
+
+          //Reads file into file and writes data to Client
+          FILE *file;
+          int nr = 0;
+          char buf[CHUNK_SIZE];
+          file = fopen(fileName, "rb");
+          while ((nr = fread(buf, 1, CHUNK_SIZE, file)) > 0)
+          {
+               if (write(sd, buf, nr) == -1)
+               {
+                    printf("failed to send file content\n");
+                    return;
+               }
+          }
+          printf("GET SUCCESSFUL\n");
+     }
+     else
+     {
+          nw = write(sd, &ackfileDoesNotExist, sizeof(ackfileDoesNotExist)); //writes ackfileDoesNotExist to client
+          printf("FILE DOES NOT EXIST\n");
+     }
+}
+
+void serve_a_client(int sd)
+{
+     char opcode;
+
+     /*Reads OPCODES and runs appropriate functions*/
+     while (read(sd, &opcode, sizeof(opcode)))
+     {
+
+          if (opcode == 'P')
+          {
+               serve_put(sd);
+          }
+          else if (opcode == 'G')
+          {
+               serve_get(sd);
+          }
+          else if (opcode == 'Q')
+          {
+               break;
+          }
+     }
+
+     return;
 }
 int main()
 {
-     int sd, nsd, n, cli_addrlen;  pid_t pid;
-     struct sockaddr_in ser_addr, cli_addr; 
-     
+     int sd, nsd, n, cli_addrlen;
+     pid_t pid;
+     struct sockaddr_in ser_addr, cli_addr;
+
      /* turn the program into a daemon */
-     daemon_init();  
+     daemon_init();
 
      /* set up listening socket sd */
-     if ((sd = socket(PF_INET, SOCK_STREAM, 0)) < 0) {
-          perror("server:socket");  
+     if ((sd = socket(PF_INET, SOCK_STREAM, 0)) < 0)
+     {
+          perror("server:socket");
           exit(1);
-     } 
+     }
 
      /* build server Internet socket address */
      bzero((char *)&ser_addr, sizeof(ser_addr));
      ser_addr.sin_family = AF_INET;
      ser_addr.sin_port = htons(SERV_TCP_PORT);
-     ser_addr.sin_addr.s_addr = htonl(INADDR_ANY); 
+     ser_addr.sin_addr.s_addr = htonl(INADDR_ANY);
      /* note: accept client request sent to any one of the
         network interface(s) on this host. 
      */
 
      /* bind server address to socket sd */
-     if (bind(sd, (struct sockaddr *) &ser_addr, sizeof(ser_addr))<0){
-          perror("server bind");  
+     if (bind(sd, (struct sockaddr *)&ser_addr, sizeof(ser_addr)) < 0)
+     {
+          perror("server bind");
           exit(1);
      }
 
      /* become a listening socket */
      listen(sd, 5);
 
-     while (1) {
+     while (1)
+     {
 
           /* wait to accept a client request for connection */
           cli_addrlen = sizeof(cli_addr);
-          nsd = accept(sd, (struct sockaddr *) &cli_addr, (socklen_t *)&cli_addrlen);
-          if (nsd < 0)  
+          nsd = accept(sd, (struct sockaddr *)&cli_addr, (socklen_t *)&cli_addrlen);
+          if (nsd < 0)
           {
-               if (errno == EINTR)   /* if interrupted by SIGCHLD */
+               if (errno == EINTR) /* if interrupted by SIGCHLD */
                     continue;
                printf("Knee grow\n");
-               perror("server:accept");  
-               
+               perror("server:accept");
+
                exit(1);
-          } 
+          }
 
           /* create a child process to handle this client */
-          if ((pid=fork()) <0) {
-              perror("fork"); exit(1);
-          } else if (pid > 0) { 
-              close(nsd);
-              continue; /* parent to wait for next client */
+          if ((pid = fork()) < 0)
+          {
+               perror("fork");
+               exit(1);
+          }
+          else if (pid > 0)
+          {
+               close(nsd);
+               continue; /* parent to wait for next client */
           }
 
           /* now in child, serve the current client */
-          
-          close(sd); 
+
+          close(sd);
           //menuDisplay(nsd,n);
-          serve_a_client(nsd); 
+          serve_a_client(nsd);
           exit(0);
      }
 }
